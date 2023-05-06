90fb92ff3c689c2af143d15adcf2a5a947751e38
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-05-06 12:02:56 -0500

    finished added functions

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function createTable() {
   for (let i = 0; i < librarySize; i += 1) {
     row = document.createElement("tr");
     propertyArray = Object.values(myLibrary[i]);
-    for (let x = 0; x < propertyArray.length; x += 1) {
+    for (let x = 0; x < propertyArray.length - 1; x += 1) {
       data = document.createElement("td");
       data.textContent = propertyArray[x];
       row.appendChild(data);
@@ -43,7 +43,32 @@ function createTable() {
     body.appendChild(row);
   }
 }
+
 addBookToLibrary(firstBook);
 createTable();
 addBookToLibrary(secondBook);
 createTable();
+
+/* Forms */
+const addBookButton = document.querySelector(".add-book");
+addBookButton.addEventListener("click", () => {
+  document.getElementById("myForm").style.display = "block";
+});
+
+const closeButton = document.querySelector(".cancel");
+closeButton.addEventListener("click", () => {
+  document.getElementById("myForm").style.display = "none";
+});
+
+const submitButton = document.querySelector(".submit");
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const title = document.getElementById("book-name").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").value;
+  const book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
+  document.getElementById("myForm").style.display = "none";
+  createTable();
+});
